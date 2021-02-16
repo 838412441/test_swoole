4ba3f9099be403b0ad021314a4f95d283d59dd0c
838412441 <838412441@qq.com>
2021-02-16 16:21:17 +0800

    no message

--- app/Console/Commands/Swoole.php
+++ app/Console/Commands/Swoole.php
@@ -30,11 +30,11 @@ class Swoole extends Command
     public function __construct()
     {
         parent::__construct();
-        $this->server = new Swoole\WebSocket\Server("0.0.0.0", 9501);
-        $this->server->on('open', function (swoole_websocket_server $server, $request) {
+        $this->server = new \Swoole\WebSocket\Server("0.0.0.0", 9501);
+        $this->server->on('open', function (\swoole_websocket_server $server, $request) {
             echo "server: handshake success with fd{$request->fd}\n";
         });
-        $this->server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
+        $this->server->on('message', function (\Swoole\WebSocket\Server $server, $frame) {
             echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
             $server->push($frame->fd, "this is server");
         });
