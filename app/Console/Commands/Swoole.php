4b5621b536a31db453d00440d4b15dc21050ba67
838412441 <838412441@qq.com>
2021-02-16 16:09:09 +0800

    no message

--- app/Console/Commands/Swoole.php
+++ app/Console/Commands/Swoole.php
@@ -19,6 +19,8 @@ class Swoole extends Command
      * @var string
      */
     protected $description = 'Command description';
+    protected $url = "ws://175.24.185.52:9501";
+    protected $server;
 
     /**
      * Create a new command instance.
@@ -28,6 +30,28 @@ class Swoole extends Command
     public function __construct()
     {
         parent::__construct();
+//        $this->server = new Swoole\WebSocket\Server("0.0.0.0", 9501);
+//        $this->server->on('open', function (swoole_websocket_server $server, $request) {
+//            echo "server: handshake success with fd{$request->fd}\n";
+//        });
+//        $this->server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
+//            echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+//            $server->push($frame->fd, "this is server");
+//        });
+//        $this->server->on('close', function ($ser, $fd) {
+//            echo "client {$fd} closed\n";
+//        });
+//        $this->server->on('request', function ($request, $response) {
+//            // 接收http请求从get获取message参数的值，给用户推送
+//            // $this->server->connections 遍历所有websocket连接用户的fd，给所有用户推送
+//            foreach ($this->server->connections as $fd) {
+//                // 需要先判断是否是正确的websocket连接，否则有可能会push失败
+//                if ($this->server->isEstablished($fd)) {
+//                    $this->server->push($fd, $request->get['message']);
+//                }
+//            }
+//        });
+//        $this->server->start();
     }
 
     /**
@@ -37,6 +61,7 @@ class Swoole extends Command
      */
     public function handle()
     {
-        dd("测试命令使用");
+        dd($this->url);
     }
+
 }
